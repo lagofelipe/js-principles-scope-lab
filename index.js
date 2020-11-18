dfe0f4307f44f5ba5da26cb26d0d3b19fd772340
lagofelipe <lagofelipe@gmail.com>
2020-11-17 20:53:00 -0600

    lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = "Carol Baskin";
+
+function upperCaseCustomerName (){
+  customerName =  customerName.toUpperCase()
+  };
+
+  function setBestCustomer(){
+bestCustomer = "not bob"
+  };
+
+  function overwriteBestCustomer(){
+bestCustomer = "maybe bob"
+  };
+
+  function changeLeastFavoriteCustomer (){
+      leastFavoriteCustomer = "tiger King"
+  };
